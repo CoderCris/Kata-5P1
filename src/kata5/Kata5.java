12985fae48c3ac17d176e0912c4762126a017b03
Crisamer Ortega González <crisamer.ortega@gmail.com>
2018-01-10 17:02:04 +0000

    First Commit of first version

---
+++ src/kata5/Kata5.java
@@ -0,0 +1,24 @@
+package kata5;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class Kata5 {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        String urlConnection = "jdbc:sqlite:Kata5-PRUEBA.DB";
+        Connection connection = DriverManager.getConnection(urlConnection);
+       
+        Statement statement = connection.createStatement();
+        String query = "Select * from PEOPLE";
+        ResultSet rs = statement.executeQuery(query);
+        
+        while(rs.next()) {
+            System.out.println(rs.getInt(1));
+            System.out.println(rs.getString(2));
+        }
+    }
+}
