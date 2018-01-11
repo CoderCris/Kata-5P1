d6aa5dae5b83b186419e15250311d4bc7ff261fe
Crisamer Ortega González <crisamer.ortega@gmail.com>
2018-01-11 12:19:19 +0000

    Seconmd and third Versions.

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,24 +1,38 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+
 public class Kata5 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         String urlConnection = "jdbc:sqlite:Kata5-PRUEBA.DB";
-        Connection connection = DriverManager.getConnection(urlConnection);
-       
-        Statement statement = connection.createStatement();
-        String query = "Select * from PEOPLE";
-        ResultSet rs = statement.executeQuery(query);
-        
-        while(rs.next()) {
-            System.out.println(rs.getInt(1));
-            System.out.println(rs.getString(2));
+        try (Connection connection = DriverManager.getConnection(urlConnection); Statement statement = connection.createStatement()) {
+            String query = ("CREATE TABLE IF NOT EXISTS MAILS ('Id'INTEGER PRIMARY KEY AUTOINCREMENT, 'Mail' TEXT  NOT NULL)");
+            statement.execute(query);
+            
+            String fileString = "emails.txt";
+            BufferedReader reader = new BufferedReader(new FileReader(new File (fileString)));
+            String mail;
+            int registerCount = 0;
+            
+            while((mail = reader.readLine()) != null) {
+                if(!mail.contains("@")) continue;
+                query = "INSERT INTO MAILS(mail) VALUES ('"+ mail + "')";
+                statement.executeUpdate(query);
+                registerCount++;
+            }
+            System.out.println("Se han introducido "+registerCount+" registros." );
         }
-    }
-}
+     }
+ }
